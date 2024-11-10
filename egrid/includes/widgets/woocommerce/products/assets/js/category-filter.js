9ae718ad764b8e478ce7aa4c397a2c129460df65
jangir-ritik <115213651+jangir-ritik@users.noreply.github.com>
2024-11-10 23:54:28 +0530

    Update category-filter.js
    remove uncategorised data from the selection

--- egrid/includes/widgets/woocommerce/products/assets/js/category-filter.js
+++ egrid/includes/widgets/woocommerce/products/assets/js/category-filter.js
@@ -1,59 +1,77 @@
-(function($) {
-    /**
-     * @param $scope The Widget wrapper element as a jQuery element
-     * @param $ The jQuery alias
-     */
-    var WidgetEGridProductsCategoryFilterHandler = function($scope, $) {
-        var els = $scope.find('[egrid-products-category-filter]');
-        if(els.length == 0){
-            var widgetId = $scope.data('id');
-            els = $('#egrid-products-filters-' + widgetId).find('[egrid-products-category-filter]');
-        }
-        var filters = $scope.data('filters') || {};
-        var categories = filters.categories || [];
-        els.each(function(index, el) {
-            el = $(el);
-            if (el.is('select')) {
-                el.on('change', function() {
-                    categories = el.val();
+(function ($) {
+  /**
+   * @param $scope The Widget wrapper element as a jQuery element
+   * @param $ The jQuery alias
+   */
+  var WidgetEGridProductsCategoryFilterHandler = function ($scope, $) {
+    var els = $scope.find("[egrid-products-category-filter]");
+    if (els.length == 0) {
+      var widgetId = $scope.data("id");
+      els = $("#egrid-products-filters-" + widgetId).find(
+        "[egrid-products-category-filter]"
+      );
+    }
+    var filters = $scope.data("filters") || {};
+    var categories = filters.categories || [];
+    els.each(function (index, el) {
+      el = $(el);
+      if (el.is("select")) {
+        el.on("change", function () {
+          categories = el.val();
 
-                    // Ensure categories is always an array
-                    if (categories && !Array.isArray(categories)) {
-                        categories = [categories];
-                    }
+          // Ensure categories is always an array
+          if (categories && !Array.isArray(categories)) {
+            categories = [categories];
+          }
 
-                    filters.categories = categories;
-                    $scope.data('filters', filters);
-                    $scope.data('page', 1);
-                    $scope.trigger('load');
-                });
-                el.selectWoo({
-                    minimumResultsForSearch: 5,
-                    width: '100%',
-                });
-            } else {
-                el.on('click', function(e) {
-                    e.preventDefault();
-
-                    if (el.hasClass('chosen')) {
-                        categories = [];
-                        el.removeClass('chosen');
-                    } else {
-                        categories = [el.attr("href").replace('#', '')];
-                        el.addClass('chosen');
-                    }
-
-                    filters.categories = categories;
-                    $scope.data('filters', filters);
-                    $scope.data('page', 1);
-                    $scope.trigger('load');
-                });
-            }
+          filters.categories = categories;
+          $scope.data("filters", filters);
+          $scope.data("page", 1);
+          $scope.trigger("load");
         });
-    };
+        el.selectWoo({
+          minimumResultsForSearch: 5,
+          width: "100%",
+          // Add matcher to filter out uncategorized
+          matcher: function (params, data) {
+            // Return null to skip "uncategorized" options
+            if (
+              data.text.toLowerCase() === "uncategorised" ||
+              data.text.toLowerCase() === "uncategorized"
+            ) {
+              return null;
+            }
 
-    // Make sure you run this code under Elementor.
-    $(window).on('elementor/frontend/init', function() {
-        elementorFrontend.hooks.addAction('frontend/element_ready/egrid-products.default', WidgetEGridProductsCategoryFilterHandler);
+            // Use default matcher for other options
+            return data;
+          },
+        });
+      } else {
+        el.on("click", function (e) {
+          e.preventDefault();
+
+          if (el.hasClass("chosen")) {
+            categories = [];
+            el.removeClass("chosen");
+          } else {
+            categories = [el.attr("href").replace("#", "")];
+            el.addClass("chosen");
+          }
+
+          filters.categories = categories;
+          $scope.data("filters", filters);
+          $scope.data("page", 1);
+          $scope.trigger("load");
+        });
+      }
     });
+  };
+
+  // Make sure you run this code under Elementor.
+  $(window).on("elementor/frontend/init", function () {
+    elementorFrontend.hooks.addAction(
+      "frontend/element_ready/egrid-products.default",
+      WidgetEGridProductsCategoryFilterHandler
+    );
+  });
 })(jQuery);
